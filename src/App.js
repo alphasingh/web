f01e09b246e670f1d79b67a550088fc83d8b347f
Tixa <vgec.tixa1998@gmail.com>
2021-07-17 03:13:27 +0530

    Improved CSS of Seller Login Page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Header from './Components/Header';
 import Content from './Components/Content';
 import Footer from './Components/Footer';
 import Gallery from './Components/Gallery';
-import {Route, Switch,Link, BrowserRouter} from "react-router-dom";
+import {Route, Switch, Link, BrowserRouter} from "react-router-dom";
 import Buyer from './Components/BuyerInfo';
 import Plans from './Components/Plans';
+import SellerLogin from './Components/ValidatedLoginForm';
+
 
 
 function App() {
@@ -19,9 +21,10 @@ function App() {
             
               <Switch>
                    <Route exact path="/" component={Content}/>
-                  <Route exact path='/gallery' component={Gallery}/>
+                   <Route exact path='/gallery' component={Gallery}/>
                    <Route path="/buyerinfo" component={Buyer}/>
                    <Route path="/plans" component={Plans}/>
+                   <Route path="/sellerlogin" component={SellerLogin}/>
 
               </Switch>
           <Footer/>
